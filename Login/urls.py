a01538d6252a4e33c42abab0d7e9d4be167a8c78
ricardogomezv <ricardogomezv@users.noreply.github.com>
2020-02-10 21:51:13 -0600

    actualizadoCS

--- Login/urls.py
+++ Login/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import include
 from django.contrib.auth.models import User
 # from rest_framework import routers, serilizers, viewsets
 from Login.views import CustonAuthToken
+from Login import views
 
 urlpatterns = [
-    re_path(r'^', CustonAuthToken.as_view()),
+    re_path(r'login/$', CustonAuthToken.as_view()),
     #Hola soy ricardo
+     re_path(r'example_list2/$', views.ExampleList2.as_view())
 ]
